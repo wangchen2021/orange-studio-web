0d1bbbb8df607335b3844909775175108da4f1dc
wangchen <wangchen1@blacklake.cn>
2026-03-03 17:05:17 +0800

    feat: init

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,30 +1,41 @@
-import React from 'react'
-import styles from './index.scss'
+import React from 'react';
+import styles from './index.scss';
 
-interface CardProps{
-    icon: string;
-    title: string;
-    description: string;
-    color: string;
-    gradient: string;
+interface CardProps {
+  icon: string;
+  title: string;
+  description: string;
+  color: string;
+  gradient: string;
+  pointerEvents?: () => void;
 }
 
-const Card: React.FC<CardProps> = ({icon, title, description, color, gradient}) => {
-    return (
-        <div
-            className={styles.featureCard}
-            style={{
-                '--card-color': color,
-                '--card-gradient': gradient
-            } as React.CSSProperties}
-        >
-            <div className={styles.cardGlow} />
-            <div className={styles.cardIcon}>{icon}</div>
-            <h3 className={styles.cardTitle}>{title}</h3>
-            <p className={styles.cardDescription}>{description}</p>
-            <div className={styles.cardHoverEffect} />
-        </div>
-    )
-}
+const Card: React.FC<CardProps> = ({
+  icon,
+  title,
+  description,
+  color,
+  gradient,
+  pointerEvents,
+}) => {
+  return (
+    <div
+      className={styles.featureCard}
+      style={
+        {
+          '--card-color': color,
+          '--card-gradient': gradient,
+          cursor: pointerEvents ? 'pointer' : '',
+        } as React.CSSProperties
+      }
+    >
+      <div className={styles.cardGlow} />
+      <div className={styles.cardIcon}>{icon}</div>
+      <h3 className={styles.cardTitle}>{title}</h3>
+      <p className={styles.cardDescription}>{description}</p>
+      <div className={styles.cardHoverEffect} />
+    </div>
+  );
+};
 
-export default Card
+export default Card;
